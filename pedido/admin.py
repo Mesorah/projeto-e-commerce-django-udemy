c69b4b44582e4d1f50fa0472c2c8006a3c419c38
Messorah <garabrielra@gmail.com>
2025-02-01 09:45:09 -0300

    criando o Pedido e o ItemPedido

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from pedido import models
+
+
+class ItemPedidoInline(admin.TabularInline):
+    model = models.ItemPedido
+    extra = 1
+
+
+@admin.register(models.Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    inlines = ItemPedidoInline,
+
+
+@admin.register(models.ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    pass
